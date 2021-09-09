7296deaad71c44fcd2ab2dcd31ec390b07e9fd8b
jeff-sun21 <j_hawker@sun21.co.jp>
2021-09-09 14:55:36 +0900

    sub key

--- push.js
+++ push.js
@@ -6,13 +6,13 @@ var push = require('web-push');
 //run node push to generate keys to console, then add them here
 
 let vapidKeys = {
-  publicKey: 'BP4007SMbYJj_vk6YXZs4jQmD5tOmEybP1TQLHfQtUKnlOQwKt4qUE3uFpq0Igfls4HVU0FsB38Uq4if1lFAaLY',
-  privateKey: 'quPIsgcPSn8Zd7A86G-S2y00OkNWUp4lCRBSVTds1Jg'
+    publicKey: 'BP4007SMbYJj_vk6YXZs4jQmD5tOmEybP1TQLHfQtUKnlOQwKt4qUE3uFpq0Igfls4HVU0FsB38Uq4if1lFAaLY',
+    privateKey: 'quPIsgcPSn8Zd7A86G-S2y00OkNWUp4lCRBSVTds1Jg'
 }
 
 push.setVapidDetails('mailto:j_hawker@sun21.co.uk', vapidKeys.publicKey, vapidKeys.privateKey);
 
-//sub from DB
-let sub = {}
+//TODO sub from DB
+let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/cwQARfFP-SM:APA91bG1rn3aZwYjV4WFhu8c0SYtCNGeTnPXZHYi_QTZ4QpPL9ZVxkf2gkI7ZoAd6wHFpFRk2-cmKDvPuTNGhVUQdnDiT0xQygpELKAeOO3ND2qGxpq5yHgQh2a8SsV-VWG-PwzBV0VD", "expirationTime": null, "keys": { "p256dh": "BBU_yXRikqyGnjGdVA8Bk16waSqKEZTXlGGINEi8h1z353g8QShUQF81nDo-7MihCXmlWPaAbRutFBneQOLxGuU", "auth": "9KFm8mzQ-HCGYo4tpubu2g" } }
 
 push.sendNotification(sub, 'testing push-web push manager');
