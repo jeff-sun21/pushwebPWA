bed0a33f33e4fd3d8aa1727249e2f889e21c4fdb
jeff-sun21 <j_hawker@sun21.co.jp>
2021-09-09 14:53:35 +0900

    new key

--- push.js
+++ push.js
@@ -6,8 +6,8 @@ var push = require('web-push');
 //run node push to generate keys to console, then add them here
 
 let vapidKeys = {
-    publicKey: 'BOhiidivtDwwMnsc_17NH47CxxqaqT2HixaYxzUI1liFJqSZBpwM20GaWh9aidTDUdO7RB-aZTc8j08hKhpepDw',
-    privateKey: 'B7bihMAQFWc21zkEKL0qFDWpdMIuAd7WgRqZYQ4r3y8'
+  publicKey: 'BP4007SMbYJj_vk6YXZs4jQmD5tOmEybP1TQLHfQtUKnlOQwKt4qUE3uFpq0Igfls4HVU0FsB38Uq4if1lFAaLY',
+  privateKey: 'quPIsgcPSn8Zd7A86G-S2y00OkNWUp4lCRBSVTds1Jg'
 }
 
 push.setVapidDetails('mailto:j_hawker@sun21.co.uk', vapidKeys.publicKey, vapidKeys.privateKey);
